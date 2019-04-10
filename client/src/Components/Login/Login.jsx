dd450f429777297e6645e82e86240bc693a4e636
Kyle Tranel <k_tranel@yahoo.com>
2019-04-10 05:07:34 -0500

    began login page and added some global styles

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LoginStyles from './Login.module.css';
 import * as Logo from '../shared/Logo/Logo';
+import Button from '@material-ui/core/Button';
 
 const Login = () =>{
     return (
@@ -15,6 +16,9 @@ const Login = () =>{
                     <label className={'white'} htmlFor="password">Password</label>
                     <input type="password" className="form-control" id="password" placeholder="Password"/>
                 </div>
+                <Button variant="contained" className={'yellow'}>
+                    Login
+                </Button>
             </form>
         </div>
     )

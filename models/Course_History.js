810c5122e2d8a238fe2988df1fe571f6c85c22f6
Kyle Tranel <k_tranel@yahoo.com>
2019-04-20 13:33:40 -0500

    added course history model

---
+++ models/Course_History.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports= (sequelize, Datatypes)=>{
+    return sequelize.define('course_history', {
+            id:{
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                autoIncrement:true
+            },
+            user_id:{
+                //fk from the user table
+                type: Datatypes.UUID,
+                required:true,
+                allowNull:false
+            },
+            title:{
+                type:Datatypes.STRING,
+                required:true,
+                allowNull:false
+            },
+            description:{
+                type:Datatypes.STRING,
+                required: false,
+                allowNull:true,
+            },
+            status:{
+                type:Datatypes.STRING,
+                required:true,
+                allowNull:false
+            },
+            version:{
+                type:Datatypes.INTEGER,
+                required:true,
+                allowNull:false,
+                defaultValue:0
+            },
+            updated_at:{ type: Datatypes.DATE},
+            deleted_at:{ type: Datatypes.DATE}
+        },
+        {
+            underscored:true,
+            paranoid:true
+        });
+};

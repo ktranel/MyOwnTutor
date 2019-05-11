10addfbdbcfb8afa1d3f61f818aa56f3068d6d39
Kyle Tranel <k_tranel@yahoo.com>
2019-05-11 09:31:28 -0500

    changed env variable

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const password = process.env.PASSWORD;
 const database = process.env.DATABASE;
 const host = process.env.HOST;
 const node_env = process.env.NODE_ENV;
-const session_secret = process.env.SESSION_SECRECT;
+const session_secret = process.env.SESSION_SECRET;
 
 const config = {
     dev:{

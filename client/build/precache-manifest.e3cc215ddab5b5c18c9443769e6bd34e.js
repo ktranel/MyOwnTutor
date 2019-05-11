501d3ab02d04ac7a1a6947f4199ce5905a06d3a6
Kyle Tranel <k_tranel@yahoo.com>
2019-05-11 09:56:52 -0500

    removed ignore policy from client folder preventing build from being tracked

---
+++ client/build/precache-manifest.e3cc215ddab5b5c18c9443769e6bd34e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80cf0184002a8a5d81efd4426ab279f8",
+    "url": "/static/media/library.80cf0184.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "addb2da542fc57153cc5",
+    "url": "/static/js/main.ed5ba017.chunk.js"
+  },
+  {
+    "revision": "f0e8e834f8ac296c2b6e",
+    "url": "/static/js/2.4696877f.chunk.js"
+  },
+  {
+    "revision": "addb2da542fc57153cc5",
+    "url": "/static/css/main.2be8b641.chunk.css"
+  },
+  {
+    "revision": "f0e8e834f8ac296c2b6e",
+    "url": "/static/css/2.0def52c2.chunk.css"
+  },
+  {
+    "revision": "5d25a62555c49cd58b6d2fa7492ede35",
+    "url": "/index.html"
+  }
+];
